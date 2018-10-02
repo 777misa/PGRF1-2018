d3f19c08b6b8ea2d71a81eb680b5121253030623
Milan Košťák <milankostak@seznam.cz>
2018-10-02 20:05:17 +0200

    created Renderer, added drawLine

---
+++ src/c_02_utery_18_15/main/Renderer.java
@@ -0,0 +1,66 @@
+package c_02_utery_18_15.main;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Renderer {
+
+    private BufferedImage img;
+    private Canvas canvas;
+    private static final int FPS = 1000 / 30;
+
+    public Renderer(BufferedImage img, Canvas canvas) {
+        this.img = img;
+        this.canvas = canvas;
+        setLoop();
+    }
+
+    private void setLoop() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // říct plátnu, aby zobrazil aktuální img
+                canvas.getGraphics().drawImage(img, 0, 0, null);
+                // co dělá observer - https://stackoverflow.com/a/1684476
+            }
+        }, 0, FPS);
+    }
+
+    public void clear() {
+        Graphics g = img.getGraphics();
+        g.setColor(Color.BLACK);
+        g.clearRect(0, 0, 800, 600);
+    }
+
+    public void drawPixel(int x, int y, int color) {
+        // nastavit pixel do img
+        img.setRGB(x, y, color);
+    }
+
+    public void drawLine(int x1, int y1, int x2, int y2, int color) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        float k = dy / (float) dx;
+        float q = y1 - k * x1;
+
+        if (Math.abs(k) < 1) {
+            // řídící osa X
+
+            if (x2 < x1) {
+                int x3 = x2;
+                x2 = x1;
+                x1 = x3;
+            }
+
+            for (int x = x1; x <= x2; x++) {
+                float y = k * x + q;
+                int ry = Math.round(y);
+                drawPixel(x, ry, color);
+            }
+        } else {
+            // řídící osa Y
+        }
+    }
+}
