9a33a164bbe2c145753fd9156f3ff64814bac899
Milan Košťák <milankostak@seznam.cz>
2018-10-09 20:14:57 +0200

    possible polygon solution

--- src/c_02_utery_18_15/main/Renderer.java
+++ src/c_02_utery_18_15/main/Renderer.java
@@ -93,4 +93,16 @@ public class Renderer {
             y += h;
         }
     }
+
+
+
+    /*
+    public void drawPolygon(List<Integer> points) {
+        clear();
+        drawLine(points.get(0), points.get(1), points.get(2), points.get(3));
+        i += 2;
+        // for cyklus po dvou se správným omezením
+        drawLine(points.get(i), points.get(i + 1), points.get(i + 2), points.get(i + 3));
+    }
+    */
 }
