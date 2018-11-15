2ac12a284642ccd98aa6e312c515252e529e7448
Milan Košťák <milankostak@seznam.cz>
2018-11-15 19:13:26 +0100

    organized imports

--- src/c_02_utery_18_15/controller/PgrfController3D.java
+++ src/c_02_utery_18_15/controller/PgrfController3D.java
@@ -1,8 +1,5 @@
 package c_02_utery_18_15.controller;
 
-import c_02_utery_18_15.model3D.Cube;
-import c_02_utery_18_15.model3D.Solid;
-import c_02_utery_18_15.renderer.Renderer3D;
 import c_02_utery_18_15.view.Raster;
 
 public class PgrfController3D {
