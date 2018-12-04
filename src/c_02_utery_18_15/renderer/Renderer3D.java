0e9cd928649d478aba216565a9d5709d26f139bd
Milan Košťák <milankostak@seznam.cz>
2018-12-04 20:03:25 +0100

    Clip, transformToWindow

--- src/c_02_utery_18_15/renderer/Renderer3D.java
+++ src/c_02_utery_18_15/renderer/Renderer3D.java
@@ -5,6 +5,8 @@ import c_02_utery_18_15.view.Raster;
 import transforms.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Renderer3D {
@@ -13,6 +15,7 @@ public class Renderer3D {
     private Mat4 projection;
     private Mat4 view;
     private Mat4 model;
+    private final List<Solid> solids = new ArrayList<>();
 
     public Renderer3D(Raster raster) {
         this.raster = raster;
@@ -27,7 +30,13 @@ public class Renderer3D {
         projection = new Mat4PerspRH(Math.PI / 4, Raster.HEIGHT / (float) Raster.WIDTH, 1, 200);
     }
 
-    public void draw(Solid... solids) {
+    public void add(Solid... solids) {
+        this.solids.addAll(Arrays.asList(solids));
+        repaint();
+    }
+
+    public void repaint() {
+        raster.clear();
         for (Solid solid : solids) {
             List<Point3D> vertices = solid.getVertices();
             List<Integer> indices = solid.getIndices();
@@ -36,50 +45,82 @@ public class Renderer3D {
                 int index2 = indices.get(i + 1);
                 Point3D a = vertices.get(index1);
                 Point3D b = vertices.get(index2);
-                drawLine(a, b, solid.getColor());
+                drawLine(a, b, solid.getColor()/*, solid instanceof */);
             }
         }
     }
 
-    private void drawLine(Point3D a, Point3D b, Color color) {
+    private void drawLine(Point3D a, Point3D b, Color color/*, boolean isAxis*/) {
+        /*if (isAxis) {
+            a = a.mul(view).mul(projection);
+            b = b.mul(view).mul(projection);
+        } else {*/
         a = a.mul(model).mul(view).mul(projection);
         b = b.mul(model).mul(view).mul(projection);
+
 /*
 Homogeneous Coordinates
 https://youtu.be/BwJ8sLYcPzc
 
 https://prateekvjoshi.com/2014/06/13/the-concept-of-homogeneous-coordinates/
-
 Perspective Projection
 https://youtu.be/veLvYQpxe6Y
 
 Homogeneous Coordinates
 https://youtu.be/GGG3cL6vfSc
 */
-        Vec3D v1, v2;
-        if (a.dehomog().isPresent()) {
-            v1 = a.dehomog().get();
-        } else {
-            v1 = new Vec3D(0);
+
+        // pokud nelze provést dehomogenizaci (w==0), tak ukončit průchod
+        if (!a.dehomog().isPresent() || !b.dehomog().isPresent()) {
+            return;
         }
-        if (b.dehomog().isPresent()) {
-            v2 = b.dehomog().get();
-        } else {
-            v2 = new Vec3D(0);
-        }
+        Vec3D v1 = a.dehomog().get();
+        Vec3D v2 = b.dehomog().get();
 
-        // TODO ořezání
+        // ořezání hodnot mimo X<-1,1> Y<-1,1> Z<0,1>
+        if (Math.abs(v1.getX()) > 1 || Math.abs(v2.getX()) > 1) return;
+        if (Math.abs(v1.getY()) > 1 || Math.abs(v2.getY()) > 1) return;
+        if (v1.getZ() > 1 || v1.getZ() < 0 || v2.getZ() > 1 || v2.getZ() < 0) return;
 
-        v1 = v1.mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
-        v2 = v2.mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
-
-        // TODO transformace do okna
+        v1 = transformToWindow(v1);
+        v2 = transformToWindow(v2);
 
         raster.drawLine(
                 v1.getX(), v1.getY(),
                 v2.getX(), v2.getY(),
                 color
         );
+    }
 
+    private Vec3D transformToWindow(Vec3D v) {
+        return v.mul(new Vec3D(1, -1, 1))
+                .add(new Vec3D(1, 1, 0))
+                .mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
+    }
+
+    public Mat4 getProjection() {
+        return projection;
+    }
+
+    public void setProjection(Mat4 projection) {
+        this.projection = projection;
+    }
+
+    public Mat4 getView() {
+        return view;
+    }
+
+    public void setView(Mat4 view) {
+        this.view = view;
+        repaint();
+    }
+
+    public Mat4 getModel() {
+        return model;
+    }
+
+    public void setModel(Mat4 model) {
+        this.model = model;
+        repaint();
     }
 }
--- src/c_03_utery_16_35/renderer/Renderer3D.java
+++ src/c_03_utery_16_35/renderer/Renderer3D.java
@@ -5,12 +5,15 @@ import c_03_utery_16_35.view.Raster;
 import transforms.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Renderer3D {
 
     private final Raster raster;
     private Mat4 model, view, projection;
+    private final List<Solid> solids = new ArrayList<>();
 
     public Renderer3D(Raster raster) {
         this.raster = raster;
@@ -25,56 +28,70 @@ public class Renderer3D {
         projection = new Mat4PerspRH(Math.PI / 4, Raster.HEIGHT / (float) Raster.WIDTH, 1, 200);
     }
 
-    public void draw(Solid... solids) {
-        for (int i = 0; i < solids.length; i++) {
-            List<Point3D> vertices = solids[i].getVertices();
-            List<Integer> indices = solids[i].getIndices();
+    public void add(Solid... solids) {
+        this.solids.addAll(Arrays.asList(solids));
+        repaint();
+    }
+
+    public void repaint() {
+        raster.clear();
+        for (Solid solid : solids) {
+            List<Point3D> vertices = solid.getVertices();
+            List<Integer> indices = solid.getIndices();
             for (int j = 0; j < indices.size(); j = j + 2) {
                 Point3D a = vertices.get(indices.get(j));
                 Point3D b = vertices.get(indices.get(j + 1));
-                drawLine(a, b, solids[i].getColor());
+                drawLine(a, b, solid.getColor()/*, solid instanceof*/);
             }
         }
     }
 
-    private void drawLine(Point3D a, Point3D b, Color color) {
+    private void drawLine(Point3D a, Point3D b, Color color/*, boolean isAxis*/) {
+        /*if (isAxis) {
+            a = a.mul(view).mul(projection);
+            b = b.mul(view).mul(projection);
+        } else  {*/
         a = a.mul(model).mul(view).mul(projection);
         b = b.mul(model).mul(view).mul(projection);
+
 /*
 Homogeneous Coordinates
 https://youtu.be/BwJ8sLYcPzc
 
 https://prateekvjoshi.com/2014/06/13/the-concept-of-homogeneous-coordinates/
-
 Perspective Projection
 https://youtu.be/veLvYQpxe6Y
 
 Homogeneous Coordinates
 https://youtu.be/GGG3cL6vfSc
 */
-        Vec3D v1, v2;
-        if (a.dehomog().isPresent()) {
-            v1 = a.dehomog().get();
-        } else {
-            v1 = new Vec3D(0, 0, 0);
+
+        // pokud nelze provést dehomogenizaci (w==0), tak ukončit průchod
+        if (!a.dehomog().isPresent() || !b.dehomog().isPresent()) {
+            return;
         }
-        if (b.dehomog().isPresent()) {
-            v2 = b.dehomog().get();
-        } else {
-            v2 = new Vec3D(0, 0, 0);
-        }
+        Vec3D v1 = a.dehomog().get();
+        Vec3D v2 = b.dehomog().get();
 
-        // TODO ořezání
+        // ořezání hodnot mimo X<-1,1> Y<-1,1> Z<0,1>
+        if (Math.abs(v1.getX()) > 1 || Math.abs(v2.getX()) > 1) return;
+        if (Math.abs(v1.getY()) > 1 || Math.abs(v2.getY()) > 1) return;
+        if (v1.getZ() > 1 || v1.getZ() < 0 || v2.getZ() > 1 || v2.getZ() < 0) return;
 
-        v1 = v1.mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
-        v2 = v2.mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
-
-        // TODO transformace do okna
+        // transformace do okna
+        v1 = transformToWindow(v1);
+        v2 = transformToWindow(v2);
 
         raster.drawLine(
                 v1.getX(), v1.getY(),
                 v2.getX(), v2.getY(),
                 color);
+    }
+
+    private Vec3D transformToWindow(Vec3D v) {
+        return v.mul(new Vec3D(1, -1, 1))
+                .add(new Vec3D(1, 1, 0))
+                .mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
     }
 
     public Mat4 getModel() {
@@ -83,6 +100,7 @@ https://youtu.be/GGG3cL6vfSc
 
     public void setModel(Mat4 model) {
         this.model = model;
+        repaint();
     }
 
     public Mat4 getView() {
@@ -91,6 +109,7 @@ https://youtu.be/GGG3cL6vfSc
 
     public void setView(Mat4 view) {
         this.view = view;
+        repaint();
     }
 
     public Mat4 getProjection() {
